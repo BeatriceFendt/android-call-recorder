b7bc66c9370fc4a5f3c84d9a4adc959adceb74b5
Alexey Kuznetsov <axet@me.com>
2016-09-27 10:20:04 +0300

    try default format

--- app/src/main/java/com/github/axet/audiorecorder/encoders/FormatM4A.java
+++ app/src/main/java/com/github/axet/audiorecorder/encoders/FormatM4A.java
@@ -14,9 +14,7 @@ public class FormatM4A extends MuxerMP4 {
         Map<String, MediaCodecInfo> map = findEncoder("audio/mp4");
         if (map.isEmpty())
             throw new RuntimeException("mp4 not supported");
-        MediaFormat format = new MediaFormat();
-        format.setString(MediaFormat.KEY_MIME, prefered("audio/mp4a-latm", map));
-        format.setInteger(MediaFormat.KEY_AAC_PROFILE, MediaCodecInfo.CodecProfileLevel.AACObjectHE);
+        MediaFormat format = MuxerMP4.getDefault("audio/mp4a-latm", map);
         format.setInteger(MediaFormat.KEY_SAMPLE_RATE, info.sampleRate);
         format.setInteger(MediaFormat.KEY_CHANNEL_COUNT, info.channels);
         format.setInteger(MediaFormat.KEY_BIT_RATE, 64000);
