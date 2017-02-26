1f93924358b6621d5349a43fb5b01632bb4c29ab
Alexey Kuznetsov <axet@me.com>
2017-02-26 21:09:55 +0300

    handle for slow encoding

--- app/src/main/java/com/github/axet/callrecorder/app/Storage.java
+++ app/src/main/java/com/github/axet/callrecorder/app/Storage.java
@@ -5,6 +5,7 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,15 +16,48 @@ public class Storage extends com.github.axet.audiolibrary.app.Storage {
         super(context);
     }
 
+    public boolean recordingNextPending() {
+        File tmp = getTempRecording();
+        if (tmp.exists())
+            return true;
+        File parent = tmp.getParentFile();
+        File[] ff = parent.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().startsWith(TMP_REC);
+            }
+        });
+        return ff != null && ff.length > 0;
+    }
+
+    public File getTempNextRecording() {
+        File tmp = super.getTempRecording();
+        if (tmp.exists())
+            return tmp;
+        File parent = tmp.getParentFile();
+        File[] ff = parent.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File pathname) {
+                return pathname.getName().startsWith(TMP_REC);
+            }
+        });
+        return ff != null && ff.length > 0 ? ff[0] : tmp;
+    }
+
     public File getNewFile(String phone) {
         SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(context);
         String ext = shared.getString(com.github.axet.audiolibrary.app.MainApplication.PREFERENCE_ENCODING, "");
 
-        String format = shared.getString(MainApplication.PREFERENCE_FORMAT, "%s");
+        String format = "%s";
+
+        if (phone != null && !phone.isEmpty()) {
+            format = shared.getString(MainApplication.PREFERENCE_FORMAT, format);
+            format = format.replaceAll("%p", phone);
+        }
 
         format = format.replaceAll("%T", "" + System.currentTimeMillis() / 1000);
-        format = format.replaceAll("%p", phone);
         format = format.replaceAll("%s", simple.format(new Date()));
+
 
         File parent = getStoragePath();
         if (!parent.exists()) {
