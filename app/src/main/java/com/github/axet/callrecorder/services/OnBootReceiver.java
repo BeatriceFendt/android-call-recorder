7ed01e4845bb41e51045cc0b8b5c5581eb08b1fc
Alexey Kuznetsov <axet@me.com>
2017-03-09 21:30:57 +0300

    fix reboot receiver

--- app/src/main/java/com/github/axet/callrecorder/services/OnBootReceiver.java
+++ app/src/main/java/com/github/axet/callrecorder/services/OnBootReceiver.java
@@ -3,12 +3,11 @@ package com.github.axet.callrecorder.services;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 
 public class OnBootReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
-            RecordingService.startIfEnabled(context);
-        }
+        RecordingService.startIfEnabled(context);
     }
 }
